e8813181cc28853695cd48662d3975959fc589ff
adij27 <68365846+adij27@users.noreply.github.com>
2024-11-06 21:43:06 -0700

    Model Trainer added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from textSummarizer.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipelines.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipelines.stage_04_model_trainer import DataTrainerTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -23,3 +25,28 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx========X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<<")
+    data_transformation = DataTrainerTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx========X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
